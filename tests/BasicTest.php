2736bd3a547f779302fd57326f437186d3135e82
Cal Henderson <cal@iamcal.com>
2021-01-22 19:57:58 +0000

    some very basic tests

---
+++ tests/BasicTest.php
@@ -0,0 +1,22 @@
+<?php
+
+	class BasicTest extends \PHPUnit\Framework\TestCase {
+
+		public function testParse(){
+
+			$html = '<p>This is a <b>test</b>.</p>';
+			$obj = pQuery::fromHTML($html);
+
+			$this->assertEquals('pQuery', get_class($obj));
+		}
+
+		public function testTags(){
+
+			$html = '<p>This is a <b>test</b>.</p>';
+			$obj = pQuery::fromHTML($html);
+
+			$this->assertEquals(1, count($obj->find('p')));
+			$this->assertEquals(1, count($obj->find('b')));
+		}
+
+	}
