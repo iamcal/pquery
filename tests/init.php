fe428c4905de47a78d705b8a7d9b68bfdcf36f3f
Cal Henderson <cal@iamcal.com>
2021-01-22 21:10:23 +0000

    for PHP 8, try aliasing the test class?

--- tests/init.php
+++ tests/init.php
@@ -1,3 +1,7 @@
 <?php
 	require_once __DIR__.'/../vendor/autoload.php';
-	echo "Loaded Autoloaded!\n";
+
+	if (version_compare(PHP_VERSION, '8.0', '>=')){
+
+		class_alias('\PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+	}
