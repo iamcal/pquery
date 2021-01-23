f199c45080e300a1e466ab93f95f374b6a94809b
Cal Henderson <cal@iamcal.com>
2021-01-23 01:22:35 +0000

    more test coverage

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -19,4 +19,23 @@
 			$this->assertEquals(1, count($obj->find('b')));
 		}
 
+		public function testClass(){
+
+			$html = '<p><b class="a">foo</b><b class="b">bar</b><i class="a b">baz</i></p>';
+			$obj = pQuery::fromHTML($html);
+
+			$this->assertEquals(1, count($obj->find('b.a')));
+			$this->assertEquals(2, count($obj->find('.a')));
+		}
+
+		public function testID(){
+
+			$html = '<p><b id="a">foo</b><b id="b">bar</b></p>';
+			$obj = pQuery::fromHTML($html);
+
+			$this->assertEquals(1, count($obj->find('b#a')));
+			$this->assertEquals(1, count($obj->find('#b')));
+			$this->assertEquals(0, count($obj->find('#c')));
+		}
+
 	}
